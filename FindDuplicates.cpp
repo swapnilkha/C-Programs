99e9e05b507a9ad747711f7c51a0fcc7de58749d
Swapnil Kha <swapnilkha@gmail.com>
2018-06-02 00:35:23 -0400

    Update FindDuplicates.cpp

--- FindDuplicates.cpp
+++ FindDuplicates.cpp
@@ -13,21 +13,21 @@ vector<int> findDuplicates(vector<int> v)
 	unordered_map<int,int> m; #unordered map lookup and find is O(1) average time.
 	unordered_map<int,int>:: iterator itr;
 
-  #traverse the vector 
+  	#traverse the vector 
 	for(int i = 0; i < v.size(); i++)
 	{
-    #if elem in vector is in the map, increment the key's value by 1, else set it to 0
+    		#if elem in vector is in the map, increment the key's value by 1, else set it to 0
 		if(m.find(v[i]) != m.end()) {m[v[i]] += 1;}
 		else {m[v[i]] = 0;}
 	}
 
-  #traverse the unordered map using iterator, if # of occurrences > 0, push that elem into result vector
+  	#traverse the unordered map using iterator, if # of occurrences > 0, push that elem into result vector
 	for(itr = m.begin(); itr != m.end(); itr++)
 	{
 		if(itr->second > 0) result.push_back(itr->first);
 	}
 
-  #finally return a vector containing all the duplicate values
+  	#finally return a vector containing all the duplicate values
 	return result;
 }
 
@@ -41,7 +41,8 @@ int main()
 	v.push_back(4);
 	v.push_back(4);
 	v.push_back(1);
+	
 	for(int i = 0; i < findDuplicates(v).size(); i++) 
-    cout << findDuplicates(v)[i]<< endl;
-    #in this case should print 4, 3
+    		cout << findDuplicates(v)[i]<< endl;
+    		#in this case should print 4, 3
 }
